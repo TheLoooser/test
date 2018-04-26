62563ce729cc25a140da948a289d40f9f8ca9b48
Dizzy <Dizzy@134.21.138.68>
2018-04-26 16:53:35 +0200

    fourth local repository commit

--- src/test/Test.java
+++ src/test/Test.java
@@ -11,7 +11,7 @@ public class Test {
      */
     public static void main(String[] args) {
         //simple output
-        System.out.println("Test");
+        System.out.print("Test");
     }
     
 }
